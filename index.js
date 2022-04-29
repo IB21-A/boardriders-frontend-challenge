2c32f5a8b5de8b5c83a307dbc622335a396b5473
Thom Zolghadr <thom@thomcodes.com>
2022-04-28 17:15:49 -0700

    Move swiper script to index.js

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   direction: "vertical",
-//   loop: true,
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  autoHeight: true,
+  direction: "horizontal",
+  loop: true,
 
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  speed: 400,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
-// });
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    // when window width is >= 991px
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+      grabCursor: true,
+    },
+  },
+});
