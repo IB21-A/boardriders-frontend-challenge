9e89616af06c9e38522ff35d7002ec4af8b38aa7
Thom Zolghadr <thom@thomcodes.com>
2022-04-28 17:52:19 -0700

    Add change to desktop or mobile video  on resize

--- index.js
+++ index.js
@@ -1,36 +1,64 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  autoHeight: true,
-  direction: "horizontal",
-  loop: true,
+document.addEventListener("DOMContentLoaded", function () {
+  handleResize();
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  speed: 400,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  let timeout;
+  window.onresize = function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(handleResize, 100);
+  };
 
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2.5,
-      spaceBetween: 20,
-    },
-    // when window width is >= 991px
-    991: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-      grabCursor: true,
-    },
-  },
+  loadImageSwiper();
 });
+
+const handleResize = () => {
+  updateVideo();
+};
+
+const updateVideo = () => {
+  const video = document.querySelector("video");
+  console.log(video.src);
+  if (window.innerWidth < 991) {
+    video.setAttribute("src", "/assets/video_mobile.mp4");
+  } else {
+    video.setAttribute("src", "/assets/video_desktop.mp4");
+  }
+};
+
+const loadImageSwiper = () => {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    autoHeight: true,
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    speed: 400,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      // when window width is >= 991px
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        grabCursor: true,
+      },
+    },
+  });
+};
